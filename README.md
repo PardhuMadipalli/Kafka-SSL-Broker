5d9154d7f0dced401458d06e67add5d8c52b9c30
Pardhu Madipalli <pardhu1212@gmail.com>
2020-07-13 22:55:47 +0530

    use env variables for domain and password

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # Setup kafka broker with SSL
 
+#### Dependencies
+- [keytool (Java)](https://www.oracle.com/in/java/technologies/javase/javase-jdk8-downloads.html) (version >= 1.8)
+- [openssl](https://www.openssl.org/)
+- [Kafka](https://kafka.apache.org/) (Version >= 2.0)
 
 #### Steps to start ssl enabled broker
 
-- Setup KAFKA_HOME envionment variable to the Kafka installation directory. Example `export KAFKA_HOME=/usr/home/kafka_2.11`
+- Setup KAFKA_HOME, DOMAIN and PASSWORD environment variables. 
+- KAFKA_HOME example `export KAFKA_HOME=/usr/home/kafka_2.11`
+- Properties like Kafka broker ID and zookeeper can be modified in `serverssl.properties` file.
 - Make sure that there are no other procceses running at port 9093 or 9094.
-- Modify MY_WEBSITE in `startkafkaSslBroker.sh` to the domain name of the host where the kafka broker is running.
-- Modify MY_PASSWORD to the password to be used while creating SSL certifcates and key stores.
-- Run the script using `./startkafkaSslBroker.sh`
+- All the ssl related files will be create in `$KAFKA_HOME/ssl` directory.
+- Run the script using `./startkafkaSslBroker.sh`.
 
-### Broker details
+#### Broker details
 
 - Broker SSL port is 9093 and PLAINTEXT port is 9094
--- startKafkaSslBroker.sh
+++ startKafkaSslBroker.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-MY_WEBSITE=www.mywebsite.com
-MY_PASSWORD=mypassword
-
+# Tested on Oracle Linux 7
 
 if [[ -z $KAFKA_HOME ]]; then
-	echo "KAFKA_HOME must be set"
-	exit
+	echo "KAFKA_HOME must be set."
+	exit 1
 fi
-
-
-cp $(dirname "$(readlink -f $0)")/serverssl.properties $KAFKA_HOME/config/serverssl.properties
+if [[ -z $DOMAIN ]]; then
+	echo "DOMAIN is not set, using www.mywebsite.com"
+	DOMAIN=www.mywebsite.com
+fi
+if [[ -z $PASSWORD ]]; then
+        echo "PASSWORD is not set, using abc123def"
+        PASSWORD=abc123def
+fi
 
 if [[ -d "/tmp/kafka-logs-ssl" ]]; then
 	rm -Rf /tmp/kafka-logs-ssl
@@ -18,32 +21,36 @@ fi
 
 
 if [[ -d "${KAFKA_HOME}/ssl/" ]]; then
-	echo "ssl config already exists. Setting keystore location to $KAFKA_HOME/ssl/server.keystore.jks"
-
-else
+    echo "${KAFKA_HOME}/ssl directory already exists."
+	read -p "Type Y to delete the ssl/ directory and create new keystore and certificates. Else, enter N to exit: " response
+    if [[ "${response}" != "Y" ]]; then
+        exit 0
+    else
+        echo -e "removing ssl/ directory from $KAFKA_HOME\n"
+        rm -Rf ${KAFKA_HOME}/ssl/
+    fi
+fi
 
 mkdir -p $KAFKA_HOME/ssl/
 cd $KAFKA_HOME/ssl/
 
-
-keytool -keystore server.keystore.jks -alias $MY_WEBSITE -validity 365 -genkey -keyalg RSA -dname "CN=$MY_WEBSITE, OU=orgunit, O=Organisation, L=bangalore, S=Karnataka, C=IN" -ext SAN=DNS:$MY_WEBSITE -keypass $MY_PASSWORD -storepass $MY_PASSWORD && \
-openssl req -new -x509 -keyout ca-key -out ca-cert -days 365 -passout pass:"$MY_PASSWORD" -subj "/CN=$MY_WEBSITE" && \
-keytool -keystore server.keystore.jks -alias CARoot -import -file ca-cert -storepass $MY_PASSWORD -noprompt && \
-keytool -keystore server.keystore.jks -alias $MY_WEBSITE -certreq -file cert-file -storepass $MY_PASSWORD && \
-openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days 365 -CAcreateserial -passin pass:$MY_PASSWORD && \
-keytool -keystore server.keystore.jks -alias $MY_WEBSITE -import -file cert-signed -storepass $MY_PASSWORD
-
-fi
+keytool -keystore server.keystore.jks -alias $DOMAIN -validity 365 -genkey -keyalg RSA -dname "CN=$DOMAIN, OU=orgunit, O=Organisation, L=bangalore, S=Karnataka, C=IN" -ext SAN=DNS:$DOMAIN -keypass $PASSWORD -storepass $PASSWORD && \
+openssl req -new -x509 -keyout ca-key -out ca-cert -days 365 -passout pass:"$PASSWORD" -subj "/CN=$DOMAIN" && \
+keytool -keystore server.keystore.jks -alias CARoot -import -file ca-cert -storepass $PASSWORD -noprompt && \
+keytool -keystore server.keystore.jks -alias $DOMAIN -certreq -file cert-file -storepass $PASSWORD && \
+openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days 365 -CAcreateserial -passin pass:$PASSWORD && \
+keytool -keystore server.keystore.jks -alias $DOMAIN -import -file cert-signed -storepass $PASSWORD
 
 cd $KAFKA_HOME
 KAFKA_SSL_DIR="${KAFKA_HOME}/ssl"
-echo $KAFKA_SSL_DIR
 
-sed -i "s|<WEBSITE>|${MY_WEBSITE}|g" ./config/serverssl.properties
-sed -i "s|<PASSWORD>|${MY_PASSWORD}|g" ./config/serverssl.properties
+cp $(dirname "$(readlink -f $0)")/serverssl.properties $KAFKA_HOME/config/serverssl.properties
+sed -i "s|<WEBSITE>|${DOMAIN}|g" ./config/serverssl.properties
+sed -i "s|<PASSWORD>|${PASSWORD}|g" ./config/serverssl.properties
 sed -i "s|<DIRNAME>|${KAFKA_SSL_DIR}|g" ./config/serverssl.properties
 
 $KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/serverssl.properties && \
-echo -e "***	SSL listener runs on port 9093, PLAINTEXT listener runs on port 9094	***"
-echo -e "following process runs at 9093 \nusing netstat -tulnp | grep 9093"
+echo "generated keystore file is ${KAFKA_SSL_DIR}/server.keystore.jks"
+
+echo -e "Following is the ID of the process running at 9093/TCP"
 netstat -tulnp | grep 9093
